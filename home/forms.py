ebe1bd2e98faa5f84e51b67f7eca8e7b4f95cc3d
davissontiago <davissontiagolf@gmail.com>
2026-01-10 16:31:13 -0300

    Validação de Formulários

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib import messages
 from .models import *
 
 
@@ -10,3 +11,16 @@ class CategoriaForm(forms.ModelForm):
             'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
         }
+    
+    def clean_nome(self):
+        nome = self.cleaned_data.get('nome')
+        if len(nome) < 3:
+            raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+        return nome  
+    
+    def clean_ordem(self):
+        ordem = self.cleaned_data.get('ordem')
+        if ordem <= 0:
+            raise forms.ValidationError("O campo ordem deve ser maior que zero.")
+        return ordem
+
--- home/views.py
+++ home/views.py
@@ -25,22 +25,37 @@ def form_categoria(request):
     return render(request, 'categoria/formulario.html', contexto)
 
 def editar_categoria(request, id):
-    categoria = Categoria.objects.get(pk=id)
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+     
     if request.method == 'POST':
         # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
             categoria = form.save() # save retorna o objeto salvo
+            messages.success(request, 'Operação realizada com Sucesso')
             return redirect('categoria') # redireciona para a listagem
     else:
          form = CategoriaForm(instance=categoria)
     return render(request, 'categoria/formulario.html', {'form': form,})
 
 def remover_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+    
     # Busca a categoria e a exclui do banco
     categoria = Categoria.objects.get(pk=id)
     categoria.delete()
     # Redireciona volta para a listagem
+    messages.success(request, 'Operação realizada com Sucesso')
     return redirect('categoria')
 
 def detalhes_categoria(request, id):
